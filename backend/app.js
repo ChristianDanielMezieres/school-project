d1bfb71311a5849defaec19a6b556e946209df21
chrys <chrys_mezieres@hotmail.com>
2018-12-21 22:15:32 +0100

    set test API/posts res

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,27 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First meddleware');
-  next();
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    { id: "fqsljfqls",
+      title: "title name test",
+      description: "description test right here"
+    },
+    { id: "fqskkkksds",
+      title: "title name test2",
+      description: "description test right2 here"
+    },
+    { id: "fqddddds",
+      title: "title name test3",
+      description: "description test right3 here"
+    },
+  ];
+  res.status(200).json({
+    message: ' Posts appear with success',
+    posts: posts
+  });
 });
 
-app.use((req, res, next) => {
-  res.send('hello from express!');
-});
+
 
 module.exports = app;
